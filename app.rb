e048298239784d7c815b8f76b5f7ccdad861652c
Maurius Hill <146677371+mauriushill@users.noreply.github.com>
2023-12-21 22:09:22 +0000

    updates

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ get("/") do
 
  @symbol = parsed_data
 
+ @currency = @symbol.fetch("currencies").keys
+
  erb(:homepage)
 
 end
@@ -21,7 +23,29 @@ end
 get("/:from_currency") do
   @original_currency = params.fetch("from_currency")
 
-  api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  api_url = "http://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+
+  raw_data = HTTP.get(api_url)
+
+  raw_data_string = raw_data.to_s
+ 
+  parsed_data = JSON.parse(raw_data_string)
+ 
+  @symbol = parsed_data
+ 
+  @currency = @symbol.fetch("currencies").keys
+
   
 erb(:currencylist)
+end
+
+get("/:from_currency/:to_currency") do
+  @original_currency = params.fetch("from_currency")
+  @destination_currency = params.fetch("to_currency")
+
+  api_url = "http://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+  
+ 
+  erb(:final)
+  
 end
